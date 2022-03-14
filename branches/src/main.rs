17882ce299266270926374aeabe28cb7f744f9f8
otegami <a.s.takuya1026@gmail.com>
2022-03-15 08:19:09 +0900

    learn if sysntax

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-	let number = 7;
+	let condition = true;
+	let number = if condition { 5 } else { 6 };
+	// let number = if condition { 5 } else { "six" };
+
+	println!("The value of number is: {}", number);
 
 	// if number < 5 {
 	// 	println!("condition was true");
@@ -11,13 +15,13 @@ fn main() {
 	// 	println!("number was seven")
 	// }
 
-	if number % 4 == 0 {
-		println!("number is divisible by 4");
-	} else if number % 3 == 0 {
-		println!("number is divisible by 3");
-	} else if number % 2 == 0 {
-		println!("number is divisible by 2");
-	} else {
-		println!("number is not divisible by 4, 3, or, 2");
-	}
+	// if number % 4 == 0 {
+	// 	println!("number is divisible by 4");
+	// } else if number % 3 == 0 {
+	// 	println!("number is divisible by 3");
+	// } else if number % 2 == 0 {
+	// 	println!("number is divisible by 2");
+	// } else {
+	// 	println!("number is not divisible by 4, 3, or, 2");
+	// }
 }
