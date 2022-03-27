df585199c1a07c17ce48bf8b77ceaf9871227ce8
otegami <a.s.takuya1026@gmail.com>
2022-03-27 12:13:27 +0900

    leart how to use impl in Struct

--- practical_struct/src/main.rs
+++ practical_struct/src/main.rs
@@ -4,30 +4,63 @@ struct Rectangle {
 	height: u32,
 }
 
+impl Rectangle {
+	fn square(size: u32) -> Rectangle {
+		Rectangle {
+			width: size,
+			height: size,
+		}
+	}
+
+	fn area(&self) -> u32 {
+		self.width * self.height
+	}
+
+	fn width(&self) -> bool {
+		self.width > 0
+	}
+
+	fn can_hold(&self, other: &Rectangle) -> bool {
+		self.width > other.width && self.height > other.height
+	}
+}
+
 fn main() {
-	// let rect1 = Rectangle {
-	// 	width: 30,
-	// 	height: 50,
-	// };
-
-	// println!(
-	// 	"The area of the rectangle is {} square pixedls",
-	// 	area(&rect1)
-	// );
-
-	// なぜ、 area 関数の引数で & pointer を pointer 参照するようにしないと、
-	// 下記のように参照する際に、 ownership を取られてしまい参照できなくなる
-	// println!("{:#?}", rect1);
-
-	let scale = 2;
 	let rect1 = Rectangle {
-		width: dbg!(30 * scale),
+		width: 30,
 		height: 50,
 	};
+	let rect2 = Rectangle {
+		width: 10,
+		height: 40,
+	};
+	let rect3 = Rectangle {
+		width: 60,
+		height: 45,
+	};
+	let rect4 = Rectangle::square(70);
 
-	dbg!(&rect1);
+	println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+	println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+	println!("Can rect4 hold rect3? {}", rect4.can_hold(&rect3));
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-	rectangle.width * rectangle.height
-}
+// fn main() {
+// 	let rect1 = Rectangle {
+// 		width: 30,
+// 		height: 50,
+// 	};
+
+// 	if rect1.width() {
+// 		println!("The rectangle has a nonzero width; it is {}", rect1.width);
+// 	}
+
+// 	println!(
+// 		"The area of the rectangle is {} square pixedls",
+// 		rect1.area()
+// 	);
+
+// 	// なぜ、 area 関数の引数で & pointer を pointer 参照するようにしないと、
+// 	// 下記のように参照する際に、 ownership を取られてしまい参照できなくなる
+// 	// println!("{:#?}", rect1);
+// }
