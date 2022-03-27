b8b393f3917b39c96ba6530664663bc72f9242e9
otegami <a.s.takuya1026@gmail.com>
2022-03-27 10:47:21 +0900

    learn slice

---
+++ slice/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+	// let my_string = String::from("hello world");
+
+	// let word = first_word(&my_string[0..6]);
+	// println!("{}, {}", word, my_string);
+	// let word = first_word(&my_string[..]);
+	// println!("{}, {}", word, my_string);
+	// let word = first_word(&my_string);
+	// println!("{}, {}", word, my_string);
+
+	// let my_string_literal = "hello world";
+
+	// let word = first_word(&my_string_literal[0..6]);
+	// println!("{}, {}", word, my_string_literal);
+	// let word = first_word(&my_string_literal[..]);
+	// println!("{}, {}", word, my_string_literal);
+	// let word = first_word(my_string_literal);
+	// println!("{}, {}", word, my_string_literal);
+
+	let a = [1, 2, 3, 4, 5];
+
+	let slice = &a[1..3];
+
+	assert_eq!(slice, &[2, 3]);
+}
+
+// fn first_word(s: &str) -> &str {
+// 	let bytes = s.as_bytes();
+
+// 	for (i, &item) in bytes.iter().enumerate() {
+// 		if item == b' ' {
+// 			return &s[0..i];
+// 		}
+// 	}
+
+// 	&s[..]
+// }
