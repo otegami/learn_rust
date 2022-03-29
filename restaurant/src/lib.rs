a6c7840aeac1b268fd93413e5328ab896c0d5b2b
otegami <a.s.takuya1026@gmail.com>
2022-03-30 08:47:54 +0900

    Paths for Referring to an Item in the Module Tree

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,8 +1,63 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+mod front_of_house {
+	pub mod hosting {
+		pub fn add_to_waitlist() {}
+
+		fn seat_at_table() {}
+	}
+
+	mod serving {
+		fn take_over() {}
+
+		fn serve_order() {}
+
+		fn take_payment() {}
+	}
+}
+
+mod back_of_house {
+	pub emum Appetizer {
+		Soup,
+		Salad
+	}
+
+	pub struct Breakfast {
+		pub toast: String,
+		seasonal_fruit: String,
+	}
+
+	impl Breakfast {
+		pub fn summer(toast: &str) -> Breakfast {
+			Breakfast {
+				toast: String::from(toast),
+				seasonal_fruit: String::from("peaches"),
+			}
+		}
+	}
+
+	fn fix_incorrect_order() {
+		cook_order();
+		super::serve_order();
+	}
+
+	fn cook_order() {}
+}
+
+fn serve_order() {}
+
+pub fn eat_at_breakfast() {
+	let mut meal = back_of_house::Breakfast::summer("Rye");
+
+	meal.toast = String::from("Wheat");
+	println!("I'd like {} toast please", meal.toast);
+}
+
+pub fn eat_at_restaurant() {
+	let order1 = back_of_house::Appetizer::Soup;
+	let order2 = back_of_house::Appetizer::Salad;
+
+	// Absolute path
+	crate::front_of_house::hosting::add_to_waitlist();
+
+	// Relative path
+	front_of_house::hosting::add_to_waitlist();
 }
