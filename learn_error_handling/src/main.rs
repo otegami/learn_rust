daff72dabf4c1ed48bf318fa2344f4a431239c17
otegami <a.s.takuya1026@gmail.com>
2022-04-15 09:15:50 +0900

    learning error_handling

--- learn_error_handling/src/main.rs
+++ learn_error_handling/src/main.rs
@@ -1,3 +1,38 @@
+use std::fs::File;
+// use std::io::ErrorKind;
+
+// Shortcuts for Panic on Error: unwrap and expect
 fn main() {
-    println!("Hello, world!");
+	// let f = File::open("hello.txt").unwrap();
+	let f = File::open("hello.txt").expect("Failed to open hello.txt");
 }
+
+// // Recoverable Errors with Result
+// fn main() {
+// 	let f = File::open("hello.txt");
+
+// 	let f = match f {
+// 		Ok(file) => file,
+// 		Err(error) => match error.kind() {
+// 			ErrorKind::NotFound => match File::create("hello.txt") {
+// 				Ok(fc) => fc,
+// 				Err(e) => panic!("Problem creating the file: {:?}", e)
+// 			},
+// 			other_error => {
+// 				panic!("Problem opening the file: {:?}", other_error)
+// 			}
+// 		}
+// 	};
+// }
+
+// // Using a panic! Backtrace
+// fn main() {
+// 	let v = vec![1, 2, 3];
+
+// 	v[99];
+// }
+
+// // Unrecoverable Errors with panic!
+// fn main() {
+//   panic!("crash and burn");
+// }
