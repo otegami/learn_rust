ae426434727cb11350d65171e58ee40e68bf9aef
otegami <a.s.takuya1026@gmail.com>
2022-04-16 09:59:41 +0900

    learn Recoverable Errors with Result

--- learn_error_handling/src/main.rs
+++ learn_error_handling/src/main.rs
@@ -1,11 +1,60 @@
+use std::error::Error;
 use std::fs::File;
+// use std::fs;
+// use std::io;
+// use std::fs::File;
+// use std::io::Read;
 // use std::io::ErrorKind;
 
-// Shortcuts for Panic on Error: unwrap and expect
-fn main() {
-	// let f = File::open("hello.txt").unwrap();
-	let f = File::open("hello.txt").expect("Failed to open hello.txt");
+// Where The ? Operator Can Be Used
+fn main() -> Result<(), Box<dyn Error>> {
+	File::open("hello.txt")?;
+	Ok(())
 }
+
+fn last_char_of_first_line(text: &str) -> Option<char> {
+	text.lines().next()?.chars().last()
+}
+
+// Propagating Errors
+// fn read_username_from_file() -> Result<String, io::Error> {
+// 	fs::read_to_string("hello.txt")
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+// 	let mut s = String::new();
+// 	File::open("hello.txt")?.read_to_string(&mut s)?;
+// 	Ok(s)
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+// 	let mut f = File::open("hello.txt")?;
+// 	let mut s = String::new();
+// 	f.read_to_string(&mut s)?;
+// 	Ok(s)
+// }
+
+// fn read_username_from_file() -> Result<String, io::Error> {
+// 	let f = File::open("hello.txt");
+
+// 	let mut f = match f {
+// 		Ok(file) => file,
+// 		Err(e) => return Err(e)
+// 	};
+
+// 	let mut s = String::new();
+
+// 	match f.read_to_string(&mut s) {
+// 		Ok(_) => Ok(s),
+// 		Err(e) => Err(e)
+// 	}
+// }
+
+// // Shortcuts for Panic on Error: unwrap and expect
+// fn main() {
+// 	// let f = File::open("hello.txt").unwrap();
+// 	let f = File::open("hello.txt").expect("Failed to open hello.txt");
+// }
 
 // // Recoverable Errors with Result
 // fn main() {
