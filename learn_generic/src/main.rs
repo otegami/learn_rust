d2653a274772ac8b27cdb899c9e2253716e160a1
otegami <a.s.takuya1026@gmail.com>
2022-04-16 11:05:09 +0900

    wip learn Generic Data Types

--- learn_generic/src/main.rs
+++ learn_generic/src/main.rs
@@ -1,6 +1,16 @@
-// Removing Duplication by Extracting a Function
+fn largest_i32(list: &[i32]) -> i32 {
+	let mut largest = list[0];
 
-fn largest(list: &[i32]) -> i32 {
+	for &item in list {
+		if item > largest {
+			largest = item;
+		}
+	}
+
+	largest
+}
+
+fn largest_char(list: &[char]) -> char {
 	let mut largest = list[0];
 
 	for &item in list {
@@ -13,16 +23,41 @@ fn largest(list: &[i32]) -> i32 {
 }
 
 fn main() {
-	let number_list = vec![34, 50, 25, 100, 65];
-	let result = largest(&number_list);
+	let numnber_list = vec![34, 50, 25, 100, 65];
 
+	let result = largest_i32(&numnber_list);
 	println!("The largest number is {}", result);
 
-	let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-	let result = largest(&number_list);
+	let char_list = vec!['y', 'm', 'a', 'q'];
+	let result = largest_char(&char_list);
 
-	println!("The largest number is {}", result);
+	println!("The largest char is {}", result)
 }
+
+// // Removing Duplication by Extracting a Function
+// fn largest(list: &[i32]) -> i32 {
+// 	let mut largest = list[0];
+
+// 	for &item in list {
+// 		if item > largest {
+// 			largest = item;
+// 		}
+// 	}
+
+// 	largest
+// }
+
+// fn main() {
+// 	let number_list = vec![34, 50, 25, 100, 65];
+// 	let result = largest(&number_list);
+
+// 	println!("The largest number is {}", result);
+
+// 	let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+// 	let result = largest(&number_list);
+
+// 	println!("The largest number is {}", result);
+// }
 
 // fn main() {
 // 	let number_list = vec![34, 50, 25, 100, 65];
