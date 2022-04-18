6569398911ba41c7f4080e4865045e6f6482899e
otegami <a.s.takuya1026@gmail.com>
2022-04-19 07:31:23 +0900

    Fixed the largest Function with Trait Bounds

--- learn_trait/src/lib.rs
+++ learn_trait/src/lib.rs
@@ -1,37 +1,60 @@
-pub trait Summary {
-	fn summarize_author(&self) -> String;
-	fn summarize(&self) -> String {
-		String::from("(Read more...)")
+use std::fmt::Display;
+
+struct Pair<T> {
+	x: T,
+	y: T,
+}
+
+impl<T> Pair<T> {
+	fn new(x: T, y: T) -> Self {
+		Self { x, y }
 	}
 }
-pub struct NewsArticle {
-	pub headline: String,
-	pub location: String,
-	pub author: String,
-	pub content: String,
-}
-impl Summary for NewsArticle {
-	fn summarize_author(&self) -> String {
-		format!("{}", self.author)
-	}
-	fn summarize(&self) -> String {
-		format!("{}, by {} ({})", self.headline, self.author, self.location)
+
+impl<T: Display + PartialOrd> Pair<T> {
+	fn cmp_display(&self) {
+		if self.x >= self.y {
+			println!("The largest member is x = {}", self.x);
+		} else {
+			println!("The largest member is y = {}", self.y);
+		}
 	}
 }
-pub struct Tweet {
-	pub username: String,
-	pub content: String,
-	pub reply: bool,
-	pub retweet: bool,
-}
-impl Summary for Tweet {
-	fn summarize_author(&self) -> String {
-		format!("@{}", self.username)
-	}
-	fn summarize(&self) -> String {
-		format!("{}: {}", self.username, self.content)
-	}
-}
+
+// pub trait Summary {
+// 	fn summarize_author(&self) -> String;
+// 	fn summarize(&self) -> String {
+// 		String::from("(Read more...)")
+// 	}
+// }
+// pub struct NewsArticle {
+// 	pub headline: String,
+// 	pub location: String,
+// 	pub author: String,
+// 	pub content: String,
+// }
+// impl Summary for NewsArticle {
+// 	fn summarize_author(&self) -> String {
+// 		format!("{}", self.author)
+// 	}
+// 	fn summarize(&self) -> String {
+// 		format!("{}, by {} ({})", self.headline, self.author, self.location)
+// 	}
+// }
+// pub struct Tweet {
+// 	pub username: String,
+// 	pub content: String,
+// 	pub reply: bool,
+// 	pub retweet: bool,
+// }
+// impl Summary for Tweet {
+// 	fn summarize_author(&self) -> String {
+// 		format!("@{}", self.username)
+// 	}
+// 	fn summarize(&self) -> String {
+// 		format!("{}: {}", self.username, self.content)
+// 	}
+// }
 
 // pub fn notify(item: &impl Summary) {
 // 	println!("Breaking news! {}", item.summarize());
