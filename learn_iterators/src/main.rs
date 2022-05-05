93ddf2063ae23ace61427585a540d49b0a7bd449
otegami <a.s.takuya1026@gmail.com>
2022-05-05 10:33:30 +0900

    Learnt how to use iterator

---
+++ learn_iterators/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+	let v1 = vec![1, 2, 3];
+
+	// let v1_iter = v1.iter().map(|x| x + 1);
+	// assert_eq!(v1_iter, vec![2, 3, 4]);
+
+	let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+	assert_eq!(v2, vec![2, 3, 4]);
+
+	// for val in v1_iter {
+	// 	println!("Got: {}", val);
+	// }
+}
+
+pub trait Iterator {
+	type Item;
+
+	fn next(&mut self) -> Option<Self::Item>;
+}
